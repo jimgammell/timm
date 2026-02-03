ed81e9affc16077253e208ca3244a401d680bf81
Jim Gammell <jgammell@purdue.edu>
2026-02-03 16:15:16 -0500

    ...

---
+++ train_resnet50_slurm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH --job-name=train-resnet50
+#SBATCH --partition=cocosys
+#SBATCH --account=cocosys
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-gpu=14
+#SBATCH --time=24:00:00
+#SBATCH --output=train-resnet50.out
+#SBATCH --error=train-resnet50.out
+
+source ~/.bashrc
+micromamba activate timm
+bash train_resnet50.sh
